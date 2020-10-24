2e454a7f4d9d36e5e6ea59675745a513b04d1af0
ScottLiao920 <scottliao920@gmail.com>
2020-10-24 21:18:26 +0800

    provide some other image morphological transformation methods & more similarity metrics

--- 4266A3.py
+++ 4266A3.py
@@ -1,10 +1,14 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def getContours(img):
     """Get contour of template or sample image"""
+    # according to opencv documentation, binary input is preferred, which can be get by applying threshold or
+    # canny edge detection
+
+    # thresholding method
     # Convert original image to gray scale
     imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Convert gray scale to binary
@@ -12,12 +16,15 @@ def getContours(img):
     # Words are white, background is black, easy for findContour function
     img_binary_inv = cv2.bitwise_not(img_binary)
     # Dilation make all 6 to form a closed loop
-    kernel = np.ones((5, 5), np.uint8)
+    kernel = np.ones((5,5), np.uint8)
     img_dilation = cv2.dilate(img_binary_inv, kernel, iterations=2)
+    img_erosion = cv2.erode(img_binary_inv, kernel, iterations=2)
+    img_opening = cv2.morphologyEx(img_binary_inv, cv2.MORPH_OPEN, kernel)
+    img_closing = cv2.morphologyEx(img_binary_inv, cv2.MORPH_CLOSE, kernel)
     # Get Contour
     contours, hierarchy = cv2.findContours(img_dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-    return contours
+    return [tmp for tmp in contours if tmp.shape[0] >= 5]
 
 
 def getTemplateCV():
@@ -71,7 +78,7 @@ def getSampleFDs():
 def rotationInvariant(fourierDesc):
     """Make fourier descriptor invariant to rotation and start point"""
     for index, value in enumerate(fourierDesc):
-        fourierDesc[index] = np.absolute(value) # Consider only absolute value
+        fourierDesc[index] = np.absolute(value)  # Consider only absolute value
 
     return fourierDesc
 
@@ -81,7 +88,7 @@ def scaleInvariant(fourierDesc):
     firstVal = fourierDesc[0]
 
     for index, value in enumerate(fourierDesc):
-        fourierDesc[index] = value / firstVal   # Divided by first value
+        fourierDesc[index] = value / firstVal  # Divided by first value
 
     return fourierDesc
 
@@ -96,16 +103,17 @@ def getLowFreqFDs(fourierDesc):
     # frequency order returned by np.fft is (0, 0.1, 0.2, 0.3, ...... , -0.3, -0.2, -0.1)
     # In transInvariant(), already remove first FD(0 frequency)
 
-    return fourierDesc[:4]
+    return fourierDesc[:3]
 
 
 def finalFD(fourierDesc):
     """Process fourier descriptors"""
+    print(len(fourierDesc))
     fourierDesc = rotationInvariant(fourierDesc)
     fourierDesc = scaleInvariant(fourierDesc)
     fourierDesc = transInvariant(fourierDesc)
     fourierDesc = getLowFreqFDs(fourierDesc)
-
+    print(len(fourierDesc))
     return fourierDesc
 
 
@@ -119,14 +127,34 @@ def match(template_FD, sample_FDs):
     for spFD in sample_FDs:
         spFD = finalFD(spFD)
         # Calculate Euclidean distance between template and sample
-        dist.append(np.linalg.norm(np.array(spFD) - np.array(template_FD)))
-        x, y, w, h = cv2.boundingRect(sampleContours[len(dist) - 1])
-        # Draw distance on image
-        distText = str(round(dist[len(dist) - 1], 2))
-        cv2.putText(imgOri, distText, (x, y - 8), font, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        tmp_arr = np.array(template_FD)
+        sam_arr = np.array(spFD)
+        cos_sim = np.dot(tmp_arr, sam_arr) / (np.linalg.norm(tmp_arr) * np.linalg.norm(sam_arr))
 
-        # if distance is less than threshold, it will be good match
-        if dist[len(dist) - 1] < distThreshold:
+        linf_norm = np.linalg.norm(tmp_arr - sam_arr, ord=np.inf)
+        l2_norm = np.linalg.norm(tmp_arr - sam_arr, ord=2)
+        l1_norm = np.linalg.norm(tmp_arr - sam_arr, ord=1)
+        l0_norm = np.linalg.norm(tmp_arr - sam_arr, ord=0)
+
+        metrics = {
+            'cosine similarity': cos_sim,
+            'l inf norm': linf_norm,
+            'l2 norm': l2_norm,
+            'l1 norm': l1_norm,
+            'l0 norm': l0_norm
+        }
+
+        dist.append(metrics['cosine similarity'])
+        x, y, w, h = cv2.boundingRect(sampleContours[len(dist) - 1])
+        cnt = 0
+        for metric_name in list(metrics.keys()):
+            # Draw distance on image
+            distText = '{}: {}'.format(metric_name, str(round(metrics[metric_name], 2)))
+            cv2.putText(imgOri, distText, (x, y - 10 - 10 * cnt), font, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cnt += 1
+
+        # if metrics matches, it will be good match
+        if metrics['cosine similarity'] >= simThreshold and metrics['l1 norm'] <= disThreshold:
             cv2.rectangle(imgOri, (x - 5, y - 5), (x + w + 5, y + h + 5), (40, 255, 0), 2)
 
 
@@ -138,9 +166,10 @@ if __name__ == '__main__':
     img_2 = cv2.imread("sample_2.jpg")
     img_c = cv2.imread("sample_c.jpg")
 
-    distThreshold = 0.06
-    rect = (64, 195, 171, 232)      # Rectangle area for 2
-    # rect = (489, 189, 134, 193)   # Rectangle area for C
+    simThreshold = 0.97
+    disThreshold = 0.20
+    rect = (64, 195, 171, 232)  # Rectangle area for 2
+    # rect = (489, 189, 134, 193)  # Rectangle area for C
     templateComVector = []
     sampleComVectors = []
     sampleContours = []
@@ -151,23 +180,23 @@ if __name__ == '__main__':
     # Get fourier descriptor
     template_FD = getTemplateFD()
     sampleFDs = getSampleFDs()
+
     # real match function
     match(template_FD, sampleFDs)
 
     # Visualize: Original Image
-    plt.figure()
+    plt.figure(figsize=(20, 20))
     plt.imshow(imgOri)
     plt.show()
 
     # Visualize: template contour
     template_contour_np = np.concatenate(template_contour)[:, 0]
     plt.figure()
-    plt.scatter(template_contour_np[:, 0], -template_contour_np[:, 1])
+    plt.scatter(template_contour_np[:, 0], -template_contour_np[:, 1], s=1)
     plt.show()
 
     # Visualize: sample contour
     sample_contour_np = np.concatenate(sample_contour)[:, 0]
     plt.figure()
-    plt.scatter(sample_contour_np[:, 0], -sample_contour_np[:, 1])
+    plt.scatter(sample_contour_np[:, 0], -sample_contour_np[:, 1], s=1)
     plt.show()
-
